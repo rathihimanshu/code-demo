62d612dc5b7ebaf3cd40db59fccf87d7931d9a28
himanshu <rathihimanshu@live.com>
2024-11-29 13:53:06 +0530

    two

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "somevpc" {
     source = "git::https://github.com/rathihimanshu/modulevpc?ref=v1.0.1"
     cidr_block = "10.0.0.0/16"
-    vpcname = "codeenv-vpcheremodified"  
+    vpcname = "codeenv-vpcplay"  
 }
