ab9fc60078b32ebe4ddaa77402a65463996ce0b6
himanshu <rathihimanshu@live.com>
2024-11-29 15:35:01 +0530

    four

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ module "somevpc" {
     cidr_block = "10.0.0.0/16"
     vpcname = "codeenv-vpcplay"  
 }
+resource "aws_instance" "s1" {
+    instance_type = "t2.medium"
+    ami = "ami-0166fe664262f664c"
+  
+}
